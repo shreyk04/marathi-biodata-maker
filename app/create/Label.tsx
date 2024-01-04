1a51163f812589fe465a85b78d2fc511b99ab6e2
chaitanya360 <chaitanyajadhav360@gmail.com>
2024-01-04 12:51:08 +0530

    adding input in select

--- app/create/Label.tsx
+++ app/create/Label.tsx
@@ -8,24 +8,43 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useState } from "react";
+import InputWithTranslator from "./InputWithTranslator";
+import { Check } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export const Label = ({ labels }: { labels: string[] }) => {
   const isEmty = labels.length == 0;
-  const [newText, setNewText] = useState("new");
+  const [newItems, setNewItems] = useState<any>([]);
+  const [newInput, setNewInput] = useState("");
+  const [value, setValue] = useState("");
   return (
-    <Select>
+    <Select value={value} onValueChange={(value: string) => setValue(value)}>
       <SelectTrigger className="w-full">
         <SelectValue placeholder={!isEmty && labels[0]} />
       </SelectTrigger>
-      <SelectContent>
+      <SelectContent className="z-[101]">
         {labels.map((title: string) => (
           <SelectItem value={title} key={title}>
             {title}
           </SelectItem>
         ))}
-        {/* <SelectItem value={newText}>
-          <Input onChange={(e) => setNewText(e.target.value)} value={newText} />
-        </SelectItem> */}
+        {newItems.map((title: string) => (
+          <SelectItem value={title} key={title}>
+            {title}
+          </SelectItem>
+        ))}
+        <div className="flex gap-2 items-center relative">
+          <Button
+            variant={"outline"}
+            onClick={() => {
+              setNewItems((prev: any) => [...prev, newInput as any]);
+              setValue(newInput);
+            }}
+          >
+            <Check size={"16"} />
+          </Button>
+          <InputWithTranslator onChange={(text: string) => setNewInput(text)} />
+        </div>
       </SelectContent>
     </Select>
   );
